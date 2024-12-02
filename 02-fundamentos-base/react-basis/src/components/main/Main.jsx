5c4031abc5bc58299ce5c28320f853af9e6f9a0e
victorluansilva <victorluansilva@gmail.com>
2024-12-02 20:02:54 -0300

    Correção do enunciado da atividade.

--- 02-fundamentos-base/react-basis/src/components/main/Main.jsx
+++ 02-fundamentos-base/react-basis/src/components/main/Main.jsx
@@ -3,12 +3,12 @@
 import './Main.css';
 import MeuObjetivoComReact from './MeuObjetivoComReact/MeuObjetivoComReact';
 
-//Atividade:
-// 1- Passe o componente 'CoreConcept' para um novo arquivo e pasta.
-// 2- Determine uma estilização específica do para que o conteúdo não passe da largura não passe de 32rem e o texto seja centralizado até 28rem de largura; 
+// Atividade:
+// 1- Passe o componente 'CoreConcept' para um novo arquivo e pasta tendo como base o que foi ensinado sobre estruturação de componentes e arquivos no React.
+// 2- Determine uma estilização específica para que o conteúdo em 'CoreConcept' não ultrapasse a largura de 32rem e que, epecificamente, o texto seja alinhado à esquerda e com até 28rem de largura;
 // 3- Passe as props 'id', 'titulo', 'conceitos' para 'CoreConcept'
-// 4- Renderize CoreConcept tendo como base o array conceitosChaves
-// 5- Tire printe de sua solução e envie o commite como entrega da atividade
+// 4- Renderize CoreConcept tendo como base o array conceitosChaves passando cada valor necessário para suas props.
+// 5- Tire print de sua solução e envie o commit como entrega da atividade
 
 const conceitosChaves = [
   {
