fafe5c3fae13b847fb1f13b0718c9b9a4552f6ab
victorluansilva <victorluansilva@gmail.com>
2024-12-03 19:25:41 -0300

    Ajustes do CSS de conceitos chaves

--- 02-fundamentos-base/react-basis/src/components/coreconcetp/CoreConcept.jsx
+++ 02-fundamentos-base/react-basis/src/components/coreconcetp/CoreConcept.jsx
@@ -2,17 +2,14 @@
 import "./CoreConcept.css";
 const CoreConcept = ({ id, titulo, conceitos }) => {
   return (
-    <>
-    <div id={`concept-${id}`} className="concept">
+    <div id={`concept-${id}`} className="concepts">
       <h2>{titulo}</h2>
       <ul id="core-concept-list">
         {conceitos.map((item, key) => {
-          return <li key={key} >{item}</li>;
+          return <li key={key}>{item}</li>;
         })}
       </ul>
     </div>
-      
-    </>
   );
 };
 
