6970dd5207f8b8dbe0855570b1f8c65ed9a8da4b
victorluansilva <victorluansilva@gmail.com>
2024-12-04 21:54:48 -0300

    Resolução da atividade 4

--- 02-fundamentos-base/react-basis/src/components/coreconcetp/CoreConcept.jsx
+++ 02-fundamentos-base/react-basis/src/components/coreconcetp/CoreConcept.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 import "./CoreConcept.css";
-const CoreConcept = ({ id, titulo, conceitos, action }) => {
+const CoreConcept = ({ id, titulo, imgSrc, conceitos, action }) => {
   return (
     <div id={`concept-${id}`} className="concepts">
       <h2>{titulo}</h2>
+      {imgSrc && <img src={imgSrc} alt="" />}
       <ul id="core-concept-list" onClick={action}>
         {conceitos.map((item, key) => {
           return <li key={key} >{item}</li>;
--- 02-fundamentos-base/react-basis/src/data.js
+++ 02-fundamentos-base/react-basis/src/data.js
@@ -1,7 +1,12 @@
+import imgComponent from './assets/atividade/react-component.webp'
+import imgEstadoEvento from './assets/atividade/event-usestate.webp'
+import imgProps from './assets/atividade/props-react.webp'
+
 export const conceitosChaves = [
     {
       id: 1,
       titulo: "Componentes",
+      imgSrc: imgComponent,
       conceitos: [
         "Componentes são pequenos blocos de código que podem ser reutilizados em vários lugares da interface.",
         "Os componentes são imutáveis, ou seja, não podem ser alterados diretamente.",
@@ -12,6 +17,7 @@ export const conceitosChaves = [
     {
       id: 2,
       titulo: "Estados e eventos",
+      imgSrc: imgEstadoEvento,
       conceitos: [
         "Os estados são dados que são mantidos pelo componente.",
         "Os estados são imutáveis, ou seja, não podem ser alterados diretamente.",
@@ -22,6 +28,7 @@ export const conceitosChaves = [
     {
       id: 3,
       titulo: "Props",
+      imgSrc: imgProps,
       conceitos: [
         "Props são dados que são passados de um componente pai para um componente filho.",
         "Os props são imutáveis, ou seja, não podem ser alterados diretamente.",
